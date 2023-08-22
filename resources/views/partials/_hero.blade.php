91817a882bba196f692f2fe7e62234df632219be
Lauren <lccockrum2c2@gmail.com>
2023-08-22 12:38:08 +0200

    Layout1

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,5 @@
+<!-- Hero -->
+<section class="relative h-50 bg-laravel flex flex-col justify-center align-center space-y-4 mb-4">
+    <img class="m-10,auto " src="{{ asset('images/logo2.png') }}" alt="" />
+
+</section>
