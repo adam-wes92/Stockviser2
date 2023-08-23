4950f40eab5be71f1dfafc3f8b0546b5a170c74c
Anna Bushueva <koxiperu@gmail.com>
2023-08-23 15:56:51 +0200

    added db-seeds for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create([
+            'first_name' => 'Banana',
+            'last_name' => 'Man', 
+            'address' => '456 Somewhere Ave.', 
+            'city' => 'Luxembourg City', 
+            'country' => 'Luxembourg',
+            'zip' => '2222', 
+            'birth_date' => '2022-08-27',
+            'phone_number' => '123-123-123',
+            'email' => 'banana.man@gmail.com',
+            'password' => 'Pa$$word123?'
+        ]);
     }
 }
